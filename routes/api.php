328faa6fec4d9e27a3902d9707842e9f6aa2e755
xkas01 <xkas2001@gmail.com>
2023-10-31 11:01:36 +0500

    Created Post resource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('change-password', [AuthController::class, 'changePassword']);
+Route::get('user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+
+Route::apiResources([
+//    'users' => UserController::class,
+    'posts' => PostController::class,
+]);
