d829c2ddf3836c2aa838ef3342be0b265437c5fb
xkas01 <xkas2001@gmail.com>
2023-10-31 18:59:30 +0500

    Working Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,63 +4,102 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
+use App\Repositories\PostRepository;
+use App\Rules\IntegerArray;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    /*public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StorePostRequest $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Post $post)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdatePostRequest $request, Post $post)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Post $post)
     {
         //
+    }*/
+
+    public function index(Request $request)
+    {
+        $pageSize = $request->page_size ?? 20;
+        $posts = Post::query()->paginate($pageSize);
+
+        return PostResource::collection($posts);
     }
+
+    public function store(Request $request, PostRepository $repository)
+    {
+        // TODO refactor this to a form request
+        $payload = $request->only([
+            'title',
+            'body',
+            'user_ids'
+        ]);
+        Validator::validate($payload, [
+            'title' => 'string|required',
+            'body' => ['array', 'required'],
+            'user_ids' => [
+                'array',
+                'required',
+                new IntegerArray(),
+            ]
+        ], [
+            'body.required' => "Please enter a value for body.",
+            'title.string' => 'Use a string',
+        ], [
+            'user_ids' => 'USER ID'
+        ]);
+
+        $created = $repository->create($payload);
+
+        return new PostResource($created);
+    }
+
+
+    public function show(Post $post)
+    {
+        return new PostResource($post);
+    }
+
+
+    public function update(Request $request, Post $post, PostRepository $repository)
+    {
+        $post = $repository->update($post, $request->only([
+            'title',
+            'body',
+            'user_ids',
+        ]));
+        return new PostResource($post);
+
+    }
+
+
+    public function destroy(Post $post, PostRepository $repository)
+    {
+        $post = $repository->forceDelete($post);
+        return new JsonResponse([
+            'data' => 'success'
+        ]);
+    }
+
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
+    protected $casts = [
+        'body' => 'array'
+    ];
+
+    public function getTitleUpperCaseAttribute(): string
+    {
+        return strtoupper($this->title);
+    }
+
+    public function setTitleAttribute($value): void
+    {
+        $this->attributes['title'] = strtolower($value);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
